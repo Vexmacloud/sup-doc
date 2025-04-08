7843f46062d02f455fc267db0589f52062cff191
Udith <udith.vexma@gmail.com>
2025-04-08 10:02:03 +0530

    Co-authored-by: udith-cloud-eng <udith-cloud-eng@users.noreply.github.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,14 +29,14 @@
               .catch(console.error);
           </script> -->
     <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-D46XYYDGDN"></script>
+<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-D46XYYDGDN"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-D46XYYDGDN');
-</script>
+</script> -->
 
     @vite('resources/js/main.ts')
 </head>
