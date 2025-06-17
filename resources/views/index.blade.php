c4f351aef8be8b067bf30b0b9c320a423159186f
Cyber Panel <support@vexmacloud.com>
2025-06-17 00:00:35 +0000

    Auto commit by CyberPanel Daily cron on 06-17-2025_00-00-33

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,15 +29,6 @@
               .then(() => console.log('Successfully registered and subscribed!'))
               .catch(console.error);
           </script> -->
-    <!-- Google tag (gtag.js) -->
-<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-D46XYYDGDN"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-D46XYYDGDN');
-</script> -->
 
     @vite('resources/js/main.ts')
 </head>
